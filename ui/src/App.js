836b2acf5073ecc45899c40a51c60314f4f3b2f4
Gabriel Barney <gabriel@Gabriels-MacBook-Pro-4.local>
2020-02-29 14:22:56 -0800

    made search bar a lil prettier

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import PropertyViewer from "./components/PropertyViewer";
 import Search from "./components/search";
@@ -8,22 +7,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a> God DAMMIT I love HACKATHONS!!!!! </a>
       </header>
       <div className="Properties">
+          <Search />
         <PropertyViewer />
-        <Search />
       </div>
     </div>
   );
