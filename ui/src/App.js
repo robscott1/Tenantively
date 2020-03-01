14b04551286248322a40ba1014e70789ff32427e
jayraavi <44631802+jayraavi@users.noreply.github.com>
2020-02-29 16:20:06 -0800

    Added search bar and basic router (#5)
    * added basic search
    
    * added basic components (bar), new page nav
    
    * added barebones search bar

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,26 +3,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import PropertyViewer from "./components/PropertyViewer";
 import Search from "./components/search";
+import PrettyColumn from "./components/PrettyColumn";
+import PrimarySearchAppBar from "./components/bar";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <li>{"                                                 "}</li>
+      <li>{"                                                 "}</li>
+      <li>{"                                                 "}</li>
+      <PrimarySearchAppBar />
       <div className="Properties">
-        <PropertyViewer />
+        <PrettyColumn />
         <Search />
       </div>
     </div>
