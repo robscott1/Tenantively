1ead09a6ecb99b03337079485568820a6c276365
Gabriel Barney <gabrielvbarney@gmail.com>
2020-03-01 06:11:52 -0800

    added some homes to db (#12)

--- ui/src/components/PropertyViewer.js
+++ ui/src/components/PropertyViewer.js
@@ -21,21 +21,24 @@ let properties = fetch(
     body: JSON.stringify({
       requestType: "GET",
       property: {
-        // id: "124",
+        id: "124",
         propertyManagerId: "321",
-        // address: "183 Hathway Avenue",
-        // currentLeaseId: "04293",
-        // description: "San Luis Obispo, 93405",
-        // size: "50 bed,  90 bed",
-        // isListed: true,
-        // leaseContracts: ["345534", "5432", "53243"],
-        // applications: ["4254232", "w5254f", "dja5432"],
-        // images: [
-        //   "https://media.architecturaldigest.com/photos/58fe517e1732ef7d3c2b9c6c/master/w_1600%2Cc_limit/eebbbf97-82ad-456b-b151-48cb92cac20c.jpg",
-        //   "https://i.insider.com/57362ea7910584cc008c2ed5?width=750&format=jpeg&auto=webp",
-        //   "https://assets.hamptons-international.com/resized/width-870/height-578/fit-crop/path-assets/picture/v1/BRBISVS2785-_images_original_nightphototwilightdiff.angle.jpg"
-        // ],
-        // leaseTerms: ["5423f", "fj32k4i2", "jfdsh324"]
+        address: "183 Hathway Avenue",
+        currentLeaseId: "04293",
+        description: "San Luis Obispo, 93405",
+        size: "50 bed,  90 bed",
+        isListed: true,
+        leaseContracts: ["345534", "54432", "53243", "43243", "33343", "34439"],
+        applications: ["4254232", "w5254f", "dja5432", "e45rt43", "rt45l09", "f09d2fi"],
+        images: [
+          "https://media.architecturaldigest.com/photos/58fe517e1732ef7d3c2b9c6c/master/w_1600%2Cc_limit/eebbbf97-82ad-456b-b151-48cb92cac20c.jpg",
+          "https://i.insider.com/57362ea7910584cc008c2ed5?width=750&format=jpeg&auto=webp",
+          "https://assets.hamptons-international.com/resized/width-870/height-578/fit-crop/path-assets/picture/v1/BRBISVS2785-_images_original_nightphototwilightdiff.angle.jpg",
+          "https://www.parentsdome.com/wp-content/uploads/2018/08/celebhome4-1.jpg",
+          "https://cdn.trendir.com/wp-content/uploads/old/house-design/okoboji-lake-house-13.jpg",
+          "https://cdn.houseplansservices.com/content/a9q3532m04494dgk5f1v5moh6n/w991.jpg?v=2"
+        ],
+        leaseTerms: ["54e23f", "fj32k4i2", "jfdsh324", "rt45d3r", "b9k3kaq", "nm6o0s2"]
       }
     })
   }
