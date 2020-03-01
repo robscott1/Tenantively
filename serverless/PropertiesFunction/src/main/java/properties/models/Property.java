f8732eace6e3fae8f94a2b31fa0ee48cbe4fef83
austin <austinsilveria@gmail.com>
2020-03-01 05:22:50 -0800

    Removed unused

--- serverless/PropertiesFunction/src/main/java/properties/models/Property.java
+++ serverless/PropertiesFunction/src/main/java/properties/models/Property.java
@@ -1,9 +1,5 @@
 package properties.models;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.util.List;
 
 public class Property {
@@ -107,15 +103,5 @@ public class Property {
     public void setLeaseTerms(List<String> leaseTerms) {
         this.leaseTerms = leaseTerms;
     }
-
-//    @Override
-//    public String toString() {
-//        ObjectMapper mapper = new ObjectMapper().enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-//        try {
-//            return mapper.writeValueAsString(this);
-//        } catch (JsonProcessingException e) {
-//            return "error";
-//        }
-//    }
 }
 
