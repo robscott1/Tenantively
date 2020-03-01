cf8277df8545ccbcf63cbad74e992906919a5683
robscott1 <47842575+robscott1@users.noreply.github.com>
2020-02-29 16:09:06 -0800

    EstablishTrustline

---
+++ XRPTransfer/target/UseCases/EstablishTrustLine.js
@@ -0,0 +1,30 @@
+function establishTrustline(tenant, manager){
+
+    const RippleAPI = require('ripple-lib').RippleAPI;
+
+    const api = new RippleAPI({
+    server: 'wss://s1.ripple.com' // Public rippled server hosted by Ripple, Inc.
+    });
+    api.on('error', (errorCode, errorMessage) => {
+    console.log(errorCode + ': ' + errorMessage);
+    });
+    api.on('connected', () => {
+    console.log('connected');
+    });
+
+    // Owner sets line of trust for tenant
+
+
+
+    //
+    api.on('disconnected', (code) => {
+    // code - [close code](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent) sent by the server
+    // will be 1000 if this was normal closure
+    console.log('disconnected, code:', code);
+    });
+    api.connect().then(() => {
+    /* insert code here */
+    }).then(() => {
+    return api.disconnect();
+    }).catch(console.error);
+}
