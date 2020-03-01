dee43ac255271afbbc4c4a09e32da3ebff0d23f6
robscott1 <47842575+robscott1@users.noreply.github.com>
2020-03-01 03:04:31 -0800

    Hoping a random payment will open up possible lines of trust

--- XRPTransfer/target/UseCases/Issuance.js
+++ XRPTransfer/target/UseCases/Issuance.js
@@ -23,3 +23,39 @@ function createPayment(source, destination){
         "destination": destination,
     }
 }
+
+var destination = bundle(rob.address, '500');
+var source = bundle(kimball.address, '500');
+
+var payment = createPayment(source, destination);
+
+
+const RippleAPI = require('ripple-lib').RippleAPI;
+
+const api = new RippleAPI({
+server: 'wss://s1.ripple.com' // Public rippled server hosted by Ripple, Inc.
+});
+api.on('error', (errorCode, errorMessage) => {
+console.log(errorCode + ': ' + errorMessage);
+});
+api.on('connected', () => {
+console.log('connected');
+});
+
+var preparedPayment = api.preparePayment(source.address, payment).then(
+                                        prepared => {console.log(prepared)});
+
+var signedPayment = api.sign(preparedPayment, source.secret);
+
+api.submit(signedPayment).then(value => {console.log('Payment completed. ID: ' + value)});
+
+api.on('disconnected', (code) => {
+// code - [close code](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent) sent by the server
+// will be 1000 if this was normal closure
+console.log('disconnected, code:', code);
+})
+api.connect().then(() => {
+console.log('boof function call')
+}).then(() => {
+return api.disconnect();
+}).catch(console.error);
