e6ba469a8c8a5c9e5d899652d31dbe601dfc175b
Vijay Raavi <vijayraavi@Vijays-MacBook.local>
2020-02-29 16:31:50 -0800

    fixed App.js after merge conflict error

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,24 +6,22 @@ import TenantPropertyView from "./components/TenantPropertyView";
 import PrettyColumn from "./components/PrettyColumn";
 import PrimarySearchAppBar from "./components/bar";
 
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
       <div className="Properties">
-          <h1> Owned Property List: </h1>
-          <TenantPropertyView />
-          <Search />
-        <PropertyViewer />
-      <li>{"                                                 "}</li>
-      <li>{"                                                 "}</li>
-      <li>{"                                                 "}</li>
-      <PrimarySearchAppBar />
-      <div className="Properties">
-        <PrettyColumn />
+        <h1> Owned Property List: </h1>
+        <li>{"                                                 "}</li>
+        <li>{"                                                 "}</li>
+        <TenantPropertyView />
         <Search />
+        <li>{"                                                 "}</li>
+        <li>{"                                                 "}</li>
+        <li>{"                                                 "}</li>
+        <PrimarySearchAppBar />
+        <div className="Properties">
+          <PrettyColumn />
+        </div>
       </div>
     </div>
   );
